303930908ea5e3491ae21cb4c20e8f87eef7117f
RyanWong <ryan-wong@shaw.ca>
2019-11-21 08:55:08 -0800

    changed waitstate from 15 to 3

--- ArcadeStick/DeviceInit_18Nov2018.c
+++ ArcadeStick/DeviceInit_18Nov2018.c
@@ -15,12 +15,13 @@ extern void DelayUs(Uint16); //LS
 
 
 void DeviceInit(void);
-
+void InitFlash(void);
 //---------------------------------------------------------------
 //  Configure Device for target Application Here
 //---------------------------------------------------------------
 void DeviceInit(void)
 {
+    InitFlash(); // change wait states
    EALLOW; // temporarily unprotect registers
 
 // LOW SPEED CLOCKS prescale register settings
@@ -250,6 +251,33 @@ void DeviceInit(void)
 	EDIS;	// restore protection of registers
 
 } // end DeviceInit()
+
+void InitFlash(void)
+{
+   EALLOW;
+   //Enable Flash Pipeline mode to improve performance
+   //of code executed from Flash.
+   FlashRegs.FOPT.bit.ENPIPE = 1;
+   //                CAUTION
+   //Minimum waitstates required for the flash operating
+   //at a given CPU rate must be characterized by TI.
+   //Refer to the datasheet for the latest information.
+   //Set the Paged Waitstate for the Flash
+   FlashRegs.FBANKWAIT.bit.PAGEWAIT = 3;
+   //Set the Random Waitstate for the Flash
+   FlashRegs.FBANKWAIT.bit.RANDWAIT = 3;
+   //Set the Waitstate for the OTP
+   FlashRegs.FOTPWAIT.bit.OTPWAIT = 5;
+   //                CAUTION
+   //ONLY THE DEFAULT VALUE FOR THESE 2 REGISTERS SHOULD BE USED
+   FlashRegs.FSTDBYWAIT.bit.STDBYWAIT = 0x01FF;
+   FlashRegs.FACTIVEWAIT.bit.ACTIVEWAIT = 0x01FF;
+   EDIS;
+   //Force a pipeline flush to ensure that the write to
+   //the last register configured occurs before returning.
+   asm(" RPT #7 || NOP");
+}
+
 
 //===============================================================
 // End of file
